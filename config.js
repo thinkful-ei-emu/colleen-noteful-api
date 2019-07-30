6759f6f7b1facaebd67d397d807a62db9add3c8a
Colleen <colleenhiggins87@gmail.com>
2019-07-30 13:42:30 -0700

    moved config file to src

--- config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable strict */
-
-module.exports = {
-  PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL:process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/noteful'
-}
