1415549dd7a5d77862e92907a9e89bc3000457b2
Colleen <colleenhiggins87@gmail.com>
2019-07-30 13:46:02 -0700

    updated logger config endpoint

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,6 @@
 /* eslint-disable strict */
 const winston = require('winston');
-const {NODE_ENV}=require('../config');
+const {NODE_ENV}=require('./config');
 
 const logger = winston.createLogger({
   level: 'info',
